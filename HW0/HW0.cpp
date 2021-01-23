8a2f0fc06a0da0d4cfd847b59275a93d23d4c183
Christopher Glass <cmglass@alaska.edu>
2021-01-22 19:45:34 -0900

    added a cpp_ftoc that when run should convert Fahrenheit to Celsius

--- HW0/HW0.cpp
+++ HW0/HW0.cpp
@@ -3,6 +3,19 @@
 #include <vector>
 #include <string>
 
+double cpp_ftoc(const char* str)
+{
+	std::stringstream fen;
+
+	double x;
+
+	fen << str;
+
+	fen >> x;
+
+	return x - 32 / (9.0 / 5.0);
+
+}
 
 int main()
 {
