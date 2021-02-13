9766795f8276eebf83fd5cb17277c2c1efab06b8
Christopher Glass <cmglass@alaska.edu>
2021-02-12 19:45:48 -0900

    started program by making a struct called Student.

---
+++ HW2a/test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include"catch.hpp"
+using std::string;
+using std::cout;
+using std::endl;
+
+struct Student
+{
+	int id;
+	string name;
+	int age;
+	double gpa;
+	int classYear;
+	int cash;
+};
