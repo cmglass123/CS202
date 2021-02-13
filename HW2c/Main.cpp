5dd4ca3ba6f38763bcfdd6354b834737d04ee689
Christopher Glass <cmglass@alaska.edu>
2021-02-12 18:03:30 -0900

    forgot to commint the last one but made a for loop to look though the array and a if statement to see if the numbers matches the ascii table numbers for the upper and lowercase value

--- HW2c/Main.cpp
+++ HW2c/Main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using std::cout;
 
-void lowerCase(char* x)
+void lowerCase(char *x)
 {
+	for (int i = 0; x[i] != '0'; i++)
+	{
+		if (x[i] <= 90 && x[i] >= 65)
+		{
 
+		}
+	}
 }
