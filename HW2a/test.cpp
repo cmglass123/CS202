4cfe5f18a9f323601064832a74a506e1408ec479
Christopher Glass <cmglass@alaska.edu>
2021-02-12 19:54:26 -0900

    made 4 test case and labeled them with the task they are doing

--- HW2a/test.cpp
+++ HW2a/test.cpp
@@ -7,6 +7,7 @@ HW2a
 #include<iostream>
 #include<string>
 #include"catch.hpp"
+#include<list>
 using std::string;
 using std::cout;
 using std::endl;
@@ -31,4 +32,24 @@ bool operator!=(const Student& a, const Student& b)
 {
 	return (a.id != b.id) || (a.name != b.name) || (a.age != b.age) || (a.gpa != b.gpa) ||
 		(a.classYear != b.classYear) || (a.cash != b.cash);
+}
+
+TEST_CASE("Queue: First in First out.")
+{
+
+}
+
+TEST_CASE("Stack: last-in First out.")
+{
+
+}
+
+TEST_CASE("Insert and Find")
+{
+
+}
+
+TEST_CASE("Print the list out")
+{
+
 }
