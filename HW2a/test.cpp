c6d9247403ca52e01f41fe0dff2d0dd7513d7243
Christopher Glass <cmglass@alaska.edu>
2021-02-12 19:50:56 -0900

    made bool operators

--- HW2a/test.cpp
+++ HW2a/test.cpp
@@ -1,3 +1,9 @@
+/*
+Christopher Glass
+CS202x 
+HW2a
+*/
+
 #include<iostream>
 #include<string>
 #include"catch.hpp"
@@ -14,3 +20,15 @@ struct Student
 	int classYear;
 	int cash;
 };
+
+bool operator==(const Student& a, const Student& b) 
+{
+	return (a.id == b.id) && (a.name == b.name) && (a.age == b.age) && (a.gpa == b.gpa) &&
+		(a.classYear == b.classYear) && (a.cash == b.cash);
+}
+
+bool operator!=(const Student& a, const Student& b)
+{
+	return (a.id != b.id) || (a.name != b.name) || (a.age != b.age) || (a.gpa != b.gpa) ||
+		(a.classYear != b.classYear) || (a.cash != b.cash);
+}
