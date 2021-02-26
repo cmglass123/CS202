4424157113b61579d3f8b95a88aac6bfe5abb855
Christopher Glass <cmglass@alaska.edu>
2021-02-26 14:49:53 -0900

    worked on the vector lineToTokens that was provided to us in the homework

--- HW3A/Main.cpp
+++ HW3A/Main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+using std::string;
 #include <vector>
 using std::vector;
 #include "token.h"
+
+vector<string> lineToTokens(const string& line)
+{
+	string temp;
+	vector<string> first;
+	int x = 0;
+	for (size_t i = 0; i <= line.size(); ++i)
+	{
+		if (line[i] != ' ')
+		{
+			temp.push_back(line[i]);
+		}
+		if (line[i] == ' ' || i == line.size())
+		{
+			first.push_back(temp);
+			temp.clear();
+			++x;
+		}
+	}
+}
