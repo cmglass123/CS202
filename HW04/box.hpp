93b7ce2430a7a18936a02a629f0adb173a213651
Christopher Glass <cmglass@alaska.edu>
2021-04-01 01:56:00 -0800

    made the box class as was told by the homework

--- HW04/box.hpp
+++ HW04/box.hpp
@@ -8,3 +8,28 @@ using std::string;
 #include <fstream>
 using std::ostream;
 
+class Box
+{
+public:
+	Box();
+
+	string type();
+
+	setWidth();
+	getWidth();
+
+	setHeight();
+	getHeight();
+
+	void print(ostream& os);
+
+	ostream& operator<<(ostream& os, const Box& b);
+
+private:
+	int _width;
+	int _height;
+	string type;
+};
+
+
+#endif // !BOX_HPP
