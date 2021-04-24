e7f4cfabcfda0ec1e03faa6c93f126ff76f9b9d5
Christopher Glass <cmglass@alaska.edu>
2021-04-24 15:45:57 -0800

    made the read and write templates for the hw

---
+++ HW05A/myreadwrite.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef MYREADWRITE_HPP
+#define MYREADWRITE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+template<class T>
+void myWrite(std::ofstream& writeFile, T& a)
+{
+
+}
+
+template<class T>
+void myRead(std::ifstream& readFile, T& a)
+{
+
+}
+
+#endif // !MYREADWRITE_hpp
