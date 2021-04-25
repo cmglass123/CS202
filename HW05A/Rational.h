6acc443c1f6e509ecd78f3c55db43879495c5a95
Christopher Glass <cmglass@alaska.edu>
2021-04-24 19:26:10 -0800

    added some of Hartman's example code to the rational hpp file

--- HW05A/Rational.h
+++ HW05A/Rational.h
@@ -4,6 +4,22 @@
 
 #include <iostream>
 
+template<typename T>
+class Rational
+{
+	template<typename U>
+	friend Rational operator+(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend Rational operator-(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend Rational operator*(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend Rational operator/(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend bool operator<(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend bool operator==(Rational<U> lhs, Rational<U> rhs);
 
+};
 
 #endif // !RATIONAL_HPP
