5100a05214e9e3004ba573a91071dba0b47c6a6e
Christopher Glass <cmglass@alaska.edu>
2021-04-24 19:54:22 -0800

    copied some more code from the example rational class into this class

--- HW05A/Rational.h
+++ HW05A/Rational.h
@@ -8,7 +8,9 @@ template<typename T>
 class Rational
 {
 	template<typename U>
-	friend Rational operator+(Rational<U> lhs, Rational<U> rhs);
+	friend std::ostream& operator<<(std::ostream& os, Rational& rhs);
+	template<typename U>
+	friend Rational operator+(Rational<T> lhs, Rational<T> rhs);
 	template<typename U>
 	friend Rational operator-(Rational<U> lhs, Rational<U> rhs);
 	template<typename U>
@@ -18,8 +20,51 @@ class Rational
 	template<typename U>
 	friend bool operator<(Rational<U> lhs, Rational<U> rhs);
 	template<typename U>
+	friend bool operator>(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend bool operator<=(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend bool operator>=(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
+	friend bool operator!=(Rational<U> lhs, Rational<U> rhs);
+	template<typename U>
 	friend bool operator==(Rational<U> lhs, Rational<U> rhs);
+
+public:
+	T _num, _den;
+
+	Rational();
+	Rational(int, int = 1);
+	Rational<T> operator+=(const Rational<T> rhs);
+	Rational<T> operator-=(const Rational<T> rhs);
+	Rational<T> operator*=(const Rational<T> rhs);
+	Rational<T> operator/=(const Rational<T> rhs);
 
 };
 
 #endif // !RATIONAL_HPP
+
+template<typename U>
+std::ostream& operator<<(std::ostream& os, Rational<U> &rhs)
+{
+	os << rhs._num;
+	if (rhs._den != 0)
+	{
+		os << "/" << rhs._den;
+		return os;
+	}
+}
+
+template<class U>
+Rational <U> operator+(Rational<U> lhs, Rational<U> rhs)
+{
+	auto temp{ lhs };
+	temp += rhs;
+	return temp;
+}
+
+template<typename U>
+Rational<U> operator-(Rational<U> lhs, Rational<U> rhs)
+{
+	return { -lhs._numerator, lhs._denominator };
+}
