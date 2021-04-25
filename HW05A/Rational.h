7f2c64f38a24989f04fa13df9305d281dc997f64
Christopher Glass <cmglass@alaska.edu>
2021-04-24 18:53:20 -0800

    finished the read and write template

---
+++ HW05A/Rational.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef RATIONAL_HPP
+#define RATIONAL_HPP
+
+#include <iostream>
+
+
+#endif // !RATIONAL_HPP
--- HW05A/myreadwrite.h
+++ HW05A/myreadwrite.h
@@ -4,18 +4,24 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
+
 
 template<class T>
 void myWrite(std::ofstream& writeFile, T& a)
 {
-
+	if (writeFile)
+	{
+		writeFile.write(reinterpret_cast<const char*>(&a), sizeof(a));
+	}
 }
 
 template<class T>
 void myRead(std::ifstream& readFile, T& a)
 {
-
+	if (readFile)
+	{
+		readFile.read(reinterpret_cast<char*>(&a), sizeof(a));
+	}
 }
 
 #endif // !MYREADWRITE_hpp
