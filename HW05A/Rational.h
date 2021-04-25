899f4e9fa026fe3ab7f1ac32c82614b431d29355
Christopher Glass <cmglass@alaska.edu>
2021-04-24 20:52:39 -0800

    almost got it. it keeps saying things are ambiguous

--- HW05A/Rational.h
+++ HW05A/Rational.h
@@ -3,14 +3,15 @@
 #define RATIONAL_HPP
 
 #include <iostream>
+#include <numeric>
 
 template<typename T>
 class Rational
 {
 	template<typename U>
-	friend std::ostream& operator<<(std::ostream& os, Rational& rhs);
+	friend std::ostream& operator<<(std::ostream& os, Rational &rhs);
 	template<typename U>
-	friend Rational operator+(Rational<T> lhs, Rational<T> rhs);
+	friend Rational operator+(Rational<U> lhs, Rational<U> rhs);
 	template<typename U>
 	friend Rational operator-(Rational<U> lhs, Rational<U> rhs);
 	template<typename U>
@@ -33,16 +34,80 @@ class Rational
 public:
 	T _num, _den;
 
+	void reduce();
+
 	Rational();
-	Rational(int, int = 1);
+	Rational(T num);
+	Rational(T num, T den);
 	Rational<T> operator+=(const Rational<T> rhs);
 	Rational<T> operator-=(const Rational<T> rhs);
 	Rational<T> operator*=(const Rational<T> rhs);
 	Rational<T> operator/=(const Rational<T> rhs);
+	Rational<T> operator-();
 
 };
 
 #endif // !RATIONAL_HPP
+
+template<typename T>
+void Rational<T>::reduce()
+{
+	auto gcd = std::gcd(_num, _den);
+	_num /= gcd;
+	_den /= gcd;
+	if (_den < 0) {
+		_num *= -1;
+		_den *= -1;
+	}
+}
+
+template<typename T>
+Rational<T>::Rational() : _num(0), _den(0)
+{}
+
+template<typename T>
+Rational<T>::Rational(T num) : _num(num), _den(1)
+{}
+
+template<typename T>
+inline Rational<T>::Rational(T num, T den): _num(num), _den(den)
+{}
+
+template<typename T>
+Rational<T> Rational<T>::operator+=(const Rational<T> rhs)
+{
+	_num = _num * rhs._den + rhs._num * _den;
+	_den *= rhs._den;
+	reduce();
+	return *this;
+}
+
+template<typename T>
+Rational<T> Rational<T>::operator-=(const Rational<T> rhs)
+{
+	*this = *this - rhs;
+	return *this;
+}
+
+template<typename T>
+Rational<T> Rational<T>::operator*=(const Rational<T> rhs)
+{
+	_num *= rhs._num;
+	_den *= rhs._den;
+	return *this;
+}
+
+template<typename T>
+Rational<T> Rational<T>::operator/=(const Rational<T> rhs)
+{
+	return *this *= {rhs._den, rhs._num};
+}
+
+template<typename T>
+inline Rational<T> Rational<T>::operator-()
+{
+	return { *this = *this * -1 };
+}
 
 template<typename U>
 std::ostream& operator<<(std::ostream& os, Rational<U> &rhs)
@@ -66,5 +131,55 @@ Rational <U> operator+(Rational<U> lhs, Rational<U> rhs)
 template<typename U>
 Rational<U> operator-(Rational<U> lhs, Rational<U> rhs)
 {
-	return { -lhs._numerator, lhs._denominator };
+	return { -lhs._num, lhs._den };
 }
+
+template<typename U>
+Rational<U> operator*(Rational<U> lhs, Rational<U> rhs)
+{
+	return lhs *= rhs;
+}
+
+template<typename U>
+Rational<U> operator/(Rational<U> lhs, Rational<U> rhs)
+{
+	return lhs /= rhs;
+}
+
+template<typename U>
+bool operator<(Rational<U> lhs, Rational<U> rhs)
+{
+	return lhs._num * rhs._den < rhs._num* lhs._den;
+}
+
+template<typename U>
+bool operator>(Rational<U> lhs, Rational<U> rhs)
+{
+	return rhs < lhs;
+}
+
+template<typename U>
+bool operator<=(Rational<U> lhs, Rational<U> rhs)
+{
+	return !(rhs > lhs);
+}
+
+template<typename U>
+bool operator>=(Rational<U> lhs, Rational<U> rhs)
+{
+	return !(rhs < lhs);
+}
+
+template<typename U>
+bool operator!=(Rational<U> lhs, Rational<U> rhs)
+{
+	return !(rhs == lhs);
+}
+
+template<typename U>
+bool operator==(Rational<U> lhs, Rational<U> rhs)
+{
+	return lhs._num == rhs._num && lhs._den == rhs._den;
+}
+
+
